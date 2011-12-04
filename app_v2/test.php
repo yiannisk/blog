39cce1c6025711e7c76f9ba74181c23572340d40
Yiannis Karadimas <ikaradimas@hotmail.com>
2011-12-04 10:06:40 +0200

    Search results post is now invoked by code, instead of id.

--- app_v2/test.php
+++
@@ -1,5 +0,0 @@
-<html>
-	<body>
-		<?php echo "[".$_SERVER["SERVER_NAME"]."]"; ?>
-	</body>
-</html>
--- scripts/view.olderPosts.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by JetBrains PhpStorm.
- * User: kl1nt
- * Date: 15/11/11
- * Time: 22:29
- * To change this template use File | Settings | File Templates.
- */
